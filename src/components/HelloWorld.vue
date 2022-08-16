e6c4d2b522af35300521af56ee083ec015918615
sam <yifang.sun@lianwei.com.cn>
2022-08-16 11:47:59 +0800

    测试ip地址

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,96 +1,23 @@
 <template>
-  <n-data-table
+  <!-- <n-data-table
     :columns="columns"
     :data="bData"
     :max-height="250"
     :scroll-x="1800"
     virtual-scroll
-  />
-  <div>{{ returnCitySN["cip"] }}</div>
+  /> -->
+  <div>ip地址为:{{ data }}</div>
 </template>
 
-<script src="https://pv.sohu.com/cityjson?ie=utf-8"></script>
-
 <script lang="ts">
-import { h, defineComponent } from 'vue'
-import type { DataTableColumns } from 'naive-ui'
-
-type RowData = {
-  key: number
-  name: string
-  age: number
-  address: string
-}
-
-const columns: DataTableColumns<RowData> = [
-  {
-    type: 'selection',
-    fixed: 'left'
-  },
-  {
-    title: 'Name',
-    key: 'name',
-    width: 200,
-    fixed: 'left'
-  },
-  {
-    title: 'Age',
-    key: 'age',
-    width: 100,
-    fixed: 'left'
-  },
-  {
-    title: 'Row',
-    key: 'row',
-    render (row, index) {
-      return h('span', ['row ', index])
-    }
-  },
-  {
-    title: 'Row1',
-    key: 'row1',
-    render (row, index) {
-      return h('span', ['row ', index])
-    }
-  },
-  {
-    title: 'Row2',
-    key: 'row2',
-    render (row, index) {
-      return h('span', ['row ', index])
-    },
-    width: 100,
-    fixed: 'right'
-  },
-  {
-    title: 'Address',
-    key: 'address',
-    width: 200,
-    fixed: 'right'
-  }
-]
-
+import { defineComponent } from "vue";
+// ts-ignore
+const data = returnCitySN["cip"];
 export default defineComponent({
-  setup () {
-    const data: RowData[] = Array.from({ length: 50000 }).map((_, index) => ({
-      key: index,
-      name: `Edward King ${index}`,
-      age: 32,
-      address: `London, Park Lane no. ${index}`
-    }))
-    let bData =[
-      {
-        name: '11akioni',
-        age: 9,
-      address: `London, Park Lane no.`,
-      key:'123213sdaqwe',
-      children:data
-      }
-    ]
+  setup() {
     return {
-      bData,
-      columns
-    }
-  }
-})
+      data,
+    };
+  },
+});
 </script>
